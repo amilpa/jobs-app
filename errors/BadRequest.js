98030aeec78c19ddef79c083328e0bb0f3005368
amilpa <amilpa2020@gmail.com>
2023-05-05 22:07:34 +0530

    formatting

--- errors/BadRequest.js
+++ errors/BadRequest.js
@@ -3,9 +3,8 @@ const { CustomAPIError } = require('./custom-error')
 
 const statusCodes = require('http-status-codes')
 
-class BadRequestError extends CustomAPIError{
-  constructor(message)
-  {
+class BadRequestError extends CustomAPIError {
+  constructor(message) {
     super(message)
     this.statusCode = statusCodes.BAD_REQUEST
   }
