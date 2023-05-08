8f7c0ba9e634bb13d613fb7572da70d4ed328f5a
amilpa <amilpa2020@gmail.com>
2023-05-08 17:23:13 +0530

    formatting

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,9 @@
 const express = require('express')
 const router = express.Router()
 
-const { registerUser , loginUser } = require('../controllers/auth') 
+const { registerUser, loginUser } = require('../controllers/auth')
 
-router.post('/register',registerUser)
-router.post('/login',loginUser)
+router.post('/register', registerUser)
+router.post('/login', loginUser)
 
 module.exports = router
